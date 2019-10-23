30348c4418b64a57d2669a5d7dd7825575d083e8
Ila Lohia <Ila Lohia ilohia2@illinois.edu>
2019-10-23 11:46:50 -0700

    fix(icons): changing icons

--- src/icons/file-icon/FileIcon.js
+++ src/icons/file-icon/FileIcon.js
@@ -1,13 +1,13 @@
 // @flow
 import * as React from 'react';
 
-import IconFileAudio from '../file/IconFileAudio';
-import IconFileBoxNote from '../file/IconFileBoxNote';
-import IconFileCode from '../file/IconFileCode';
-import IconFileDefault from '../file/IconFileDefault';
+import IconFileAudio from '../design-assets/Icon/32x32/Box/Audio';
+import IconFileBoxNote from '../design-assets/Icon/32x32/Box/BoxNote';
+import IconFileCode from '../design-assets/Icon/32x32/Box/Code';
+import IconFileDefault from '../design-assets/Icon/32x32/Box/Default';
 import IconFileDocument from '../file/IconFileDocument';
-import IconFileDwg from '../file/IconFileDwg';
-import IconFileExcelSpreadsheet from '../file/IconFileExcelSpreadsheet';
+import IconFileDwg from '../design-assets/Icon/32x32/Box/Dwg';
+import IconFileExcelSpreadsheet from "../design-assets/Icon/32x32/Box";
 import IconFileGoogleDocs from '../file/IconFileGoogleDocs';
 import IconFileGoogleSheets from '../file/IconFileGoogleSheets';
 import IconFileGoogleSlides from '../file/IconFileGoogleSlides';
@@ -20,14 +20,14 @@ import IconFilePages from '../file/IconFilePages';
 import IconFilePDF from '../file/IconFilePDF';
 import IconFilePhotoshop from '../file/IconFilePhotoshop';
 import IconFilePowerpointPresentation from '../file/IconFilePowerpointPresentation';
-import IconFilePresentation from '../file/IconFilePresentation';
-import IconFileSpreadsheet from '../file/IconFileSpreadsheet';
-import IconFileText from '../file/IconFileText';
-import IconFileThreeD from '../file/IconFileThreeD';
-import IconFileVector from '../file/IconFileVector';
-import IconFileVideo from '../file/IconFileVideo';
+import IconFilePresentation from '../design-assets/Icon/32x32/Box/Presentation';
+import IconFileSpreadsheet from '../design-assets/Icon/32x32/Box/Spreadsheet';
+import IconFileText from '../design-assets/Icon/32x32/Box/Text';
+import IconFileThreeD from '../design-assets/Icon/32x32/Box/ThreeD';
+import IconFileVector from '../design-assets/Icon/32x32/Box/Vector';
+import IconFileVideo from '../design-assets/Icon/32x32/Box/Video';
 import IconFileWordDocument from '../file/IconFileWordDocument';
-import IconFileZip from '../file/IconFileZip';
+import IconFileZip from '../design-assets/Icon/32x32/Box/Zip';
 
 const Components = {
     IconFileAudio,
